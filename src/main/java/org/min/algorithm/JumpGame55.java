279e776f027cd84048375418153261df94f27d7a
Mike-Jeong <alstlr65@gmail.com>
2025-07-15 00:04:51 +1200

    refactor : refactor 55. Jump Game (14072025) - prevent Time Limit Exceeded

--- src/main/java/org/min/algorithm/JumpGame55.java
+++ src/main/java/org/min/algorithm/JumpGame55.java
@@ -4,31 +4,16 @@ package org.min.algorithm;
 // solved at 14.07.2025 23:15
 public class JumpGame55 {
 
-    private boolean[] isVisited;
-    private int[] maximumJumpLength;
-
     public boolean canJump(int[] nums) {
-        isVisited = new boolean[nums.length];
-        maximumJumpLength = nums;
+        int reachable = 0;
 
-        movePosition(0); //initially positioned at the array's first index
-
-        return isVisited[nums.length - 1];
-    }
-
-    private void movePosition(int position) {
-        if (position >= maximumJumpLength.length) {
-            return;
+        for(int i = 0; i < nums.length; i ++) {
+            if(i > reachable) {
+                return false;
+            }
+            reachable = Math.max(reachable, i + nums[i]);
         }
 
-        if (isVisited[position]) {
-            return;
-        }
-
-        isVisited[position] = true;
-
-        for (int i = 1; i <= maximumJumpLength[position]; i++) {
-            movePosition(position + i);
-        }
+        return true;
     }
 }
