c77e02961bb91b3da96445b46c4a21feb3f5cf7d
Mike-Jeong <alstlr65@gmail.com>
2025-07-16 03:20:43 +1200

    refactor : refactor 45. Jump Game II (15072025) - prevent Time Limit Exceeded

--- src/main/java/org/min/algorithm/JumpGameII45.java
+++ src/main/java/org/min/algorithm/JumpGameII45.java
@@ -1,37 +1,23 @@
 package org.min.algorithm;
 
-import java.util.Arrays;
-
 // https://leetcode.com/problems/jump-game-ii/description/?envType=study-plan-v2&envId=top-interview-150
 // solved at 15.07.2025 23:50
 public class JumpGameII45 {
 
-    private int[] minimumNumberOfJumps;
-    private int[] maximumJumpLengths;
-
     public int jump(int[] nums) {
-        maximumJumpLengths = nums;
-        minimumNumberOfJumps = new int[nums.length];
-        Arrays.fill(minimumNumberOfJumps, Integer.MAX_VALUE);
+        int count = 0;
+        int currentEnd = 0;
+        int furthest = 0;
 
-        movePosition(0, 0); //initially positioned at the array's first index
+        for (int i = 0; i < nums.length - 1; i++) {
+            furthest = Math.max(furthest, i + nums[i]);
 
-        return minimumNumberOfJumps[nums.length - 1];
-    }
-
-    private void movePosition(int position, int count) {
-        if (position >= maximumJumpLengths.length) {
-            return;
+            if (currentEnd == i) {
+                count++;
+                currentEnd = furthest;
+            }
         }
 
-        if (minimumNumberOfJumps[position] <= count) {
-            return;
-        }
-
-        minimumNumberOfJumps[position] = count;
-
-        for (int i = 1; i <= maximumJumpLengths[position]; i++) {
-            movePosition(position + i, count + 1);
-        }
+        return count;
     }
 }
