77a8557051f8aa8b6499d07761a7b2f79be8b988
Mike-Jeong <alstlr65@gmail.com>
2025-07-18 00:00:42 +1200

    test : solve 134. Gas Station (17072025) - add test codes

--- src/test/java/org/min/algorithm/GasStation134Test.java
+++ src/test/java/org/min/algorithm/GasStation134Test.java
@@ -1,11 +1,11 @@
 package org.min.algorithm;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.min.algorithm.util.StringUtil;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GasStation134Test {
 
@@ -15,6 +15,14 @@ class GasStation134Test {
             "'1,2,3,4,5', '3,4,5,1,2', 3",
             "'2,3,4', '3,4,3', -1"
     })
-    void canCompleteCircuit(String gassString, String costsString, int answer) {
+    void canCompleteCircuit(String gasString, String costsString, int answer) {
+
+        int[] gas = StringUtil.convertStringToIntArray(gasString);
+        int[] costs = StringUtil.convertStringToIntArray(costsString);
+
+        GasStation134 gasStation134 = new GasStation134();
+        int result = gasStation134.canCompleteCircuit(gas, costs);
+
+        assertThat(result).isEqualTo(answer);
     }
 }
